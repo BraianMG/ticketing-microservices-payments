2f7da9ce71a46965dc232aa656a1a60a99ecce13
Braian <braian.gonzales77@gmail.com>
2024-07-08 20:45:35 +0200

    test order created listener

---
+++ src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCreatedEvent, OrderStatus } from '@braianmg-ticketing/common';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created-listener';
+import { Order } from '../../../models/order';
+
+// TODO: When you run this test jest says:
+// A worker process has failed to exit gracefully and has been force exited.
+// This is likely caused by tests leaking due to improper teardown.
+// Try running with --detectOpenHandles to find leaks.
+// Active timers can also cause this, ensure that .unref() was called on them.
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    expiresAt: 'alskdjf',
+    userId: 'alskdjf',
+    status: OrderStatus.Created,
+    ticket: {
+      id: 'alskdfj',
+      price: 10,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it('replicates the order info', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
