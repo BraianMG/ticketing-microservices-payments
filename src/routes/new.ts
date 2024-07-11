2b9707f8f28b59dfdece873866ea2f4b390a7d99
Braian <braian.gonzales77@gmail.com>
2024-07-11 19:05:52 +0200

    creating a charge with stripe

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from '@braianmg-ticketing/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -31,6 +32,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100, // It must be indicated in cents, that is why it is multiplied by 100
+      source: token,
+    });
 
     res.send({ success: true });
   },
