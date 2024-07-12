7ae89a2780cfc04098e25564bf5fea471bb9916c
Braian <braian.gonzales77@gmail.com>
2024-07-12 16:28:24 +0200

    testing payment creation

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@braianmg-ticketing/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -33,11 +34,17 @@ router.post(
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
-      amount: order.price * 100, // It must be indicated in cents, that is why it is multiplied by 100
+      amount: order.price * 100,
       source: token,
     });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   },
